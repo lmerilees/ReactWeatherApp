c1de00157db4710177af157734bfb53d80bf09df
lmerilees <ljm597@mail.usask.ca>
2020-05-20 15:28:06 -0600

    Initial commit with basic layout

---
+++ src/containers/Daily/Daily.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Daily extends Component {
+    render(){
+        return(
+            <div>
+                <div>Monday</div>
+                <div>Tuesday</div>
+                <div>Wednesday</div>
+                <div>Thursday</div>
+                <div>Friday</div>
+                <div>Saturday</div>
+                <div>Sunday</div>
+            </div>
+        );
+    }
+}
+
+export default Daily;
